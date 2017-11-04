b87f87bd7cfb0da6c5ac59c1e5365aec531e7863
Suzi O'Rourke <orourkes@oregonstate.edu>
2017-11-03 23:08:27 -0700

    This should actually upload all card tests and unit tests. Attempt 3.

---
+++ projects/orourkes/dominion/unittest4.c
@@ -0,0 +1,21 @@
+#include "dominion.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "rngs.h"
+#include "myasserts.h"
+
+
+int main(void) {
+
+    struct gameState G;
+    G.whoseTurn=1;
+    G.phase=1;
+    G.coins=3;
+    //printf(whoseTurn(G));
+    printf("Whose turn is player %i\n", whoseTurn(&G));
+    myassert(whoseTurn(&G)==1, "Correct Player's Turn");
+    myassert(G.phase==1, "Correct phase of play");
+    myassert(G.coins==3, "Correct number of coins");
+
+  return 0;
+}
---
+++ projects/orourkes/dominion/unittests.h
@@ -0,0 +1,13 @@
+#ifndef UNITTESTS_H_INCLUDED
+#define UNITTESTS_H_INCLUDED
+
+int test_getcost (void);
+int test_isgameover (void);
+int test_whoseturn (void);
+int test_updatecoins (void);
+int test_adventurer(void);
+int test_councilroom(void);
+int test_smithy(void);
+int test_remodel(void);
+
+#endif // UNITTESTS_H_INCLUDED
